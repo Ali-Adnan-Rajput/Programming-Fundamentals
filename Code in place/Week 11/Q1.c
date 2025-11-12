a2f84272b2a81affc20759baaf0841fac2fb6162
Ali Adnan <ali.adnan.rajput@gmail.com>
2025-11-12 22:21:25 +0500

    code

---
+++ Code in place/Week 11/Q1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+  int cube(int x){
+  	return x*x*x;
+  }
+  int main(){
+ int num;
+  	printf("Enter a number: ");
+  	scanf("%d",&num);
+  	
+  	int result= cube(num);
+  
+  	printf("Cube is = %d",result);
+  	return 0;
+  }
