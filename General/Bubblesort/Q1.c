6003562780089a1ad672a066e9594afc822c5800
Ali Adnan <ali.adnan.rajput@gmail.com>
2025-12-06 15:47:38 +0500

    code

--- General/Bubblesort/Q1.c
+++ General/Bubblesort/Q1.c
@@ -1,20 +1,24 @@
 #include<stdio.h>
 int main(){
-	int arr[5] = {500,40000,30,0,8293};
+	int arr[5] = {100,24343,32344,433,0};
 	int i,j;
 	for(i=0;i<5;i++){
 		printf("%d ", arr[i]);
 	}
 	//bubble sort
 	for(i=0;i<5-1;i++){
+		//bool flag = true;
 		for(j=0;j<5-1-i;j++){
-			if(arr[j]>arr[j+1]){
+			if(arr[j]<arr[j+1]){
 				int temp =arr[j];
 				arr[j] = arr[j+1];
-				arr[j+1] = temp;			
+				arr[j+1] = temp;
+				//flag = false;			
 			}
 		}
 	}
+	//if(flag == true) break;
+	
 	printf("\n");
 	for(i=0;i<5;i++){
 		printf("%d ", arr[i]);
